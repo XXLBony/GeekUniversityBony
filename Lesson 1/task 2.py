820b9cb435b8fc75cbd8d5110d93fcf6260a96c0
XXLBony <you@example.com>
2020-12-25 13:48:35 +0300

    ok

--- Lesson 1/task 2.py
+++ Lesson 1/task 2.py
@@ -1,4 +1,11 @@
-timeinsec = int(input('Введите время в секундах: '))
-print(str(timeinsec % 3600))
-print(str((timeinsec / 60) % 60))
-print(str(((timeinsec / 3600) % 60)) % 60)
+inputstr = input('Введите время в секундах (целое число):')
+count = 2
+while not inputstr.isdigit() and count:
+    count = count - 1
+    inputstr = input('Введите время в секундах (целое число):')
+if not count:
+    print("Необходимо ввсети целое число. Попробуйте в следующий раз.")
+    exit(0)
+timeinsec = int(inputstr)
+print(f'{timeinsec // 3600:02d}:{timeinsec // 60 % 60:02d}:'
+      + f'{timeinsec % 60:02d}')
